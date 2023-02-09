fce4f5a4e81db1b04c2b70a1b4c38f9f130b6983
Eupou <wesleywesleygomesgomes@gmail.com>
2023-02-09 19:21:49 -0300

    feat: add form so the user can add their cat to the missing list

--- switchCatStatus.js
+++ switchCatStatus.js
@@ -1,13 +1,34 @@
-let btnCatStatus = document.getElementsByClassName("btn-cat-status");
-let catInfo = document.getElementsByClassName("cat-info-wrapper");
-let catHtml;
+const btnCatStatus = document.getElementsByClassName("btn-cat-status");
+const btnFoundACat = document.getElementsByClassName("btn-found-a-cat");
+const btnFindYourCat = document.getElementsByClassName("btn-find-your-cat");
+const pageFoundACat = document.getElementsByClassName("cat-info-wrapper");
+const pageFindYourCat = document.getElementsByClassName("find-your-cat");
+const pagination = document.getElementsByClassName("pagination");
 
 function switchStatus(e) {
-  //   catHtml = catInfo.children;
-  catInfo.appendChild(catInfo);
-  console.log(e);
+  let btnClicked = e.target;
+
+  if (btnClicked == btnFindYourCat[0]) {
+    btnFindYourCat[0].classList.add("active-status-js");
+    pageFindYourCat[0].classList.remove("d-none");
+
+    btnFoundACat[0].classList.remove("active-status-js");
+    pageFoundACat[0].classList.add("d-none");
+
+    pagination[0].classList.add("d-none");
+  } else {
+    btnFindYourCat[0].classList.remove("active-status-js");
+    pageFindYourCat[0].classList.add("d-none");
+
+    btnFoundACat[0].classList.add("active-status-js");
+    pageFoundACat[0].classList.remove("d-none");
+
+    pagination[0].classList.remove("d-none");
+  }
 }
 
-Array.from(btnCatStatus).forEach((ele) => {
-  ele.addEventListener("click", switchStatus);
+Array.from(btnCatStatus).forEach((ele, i) => {
+  if (i != 2) {
+    ele.addEventListener("click", switchStatus);
+  }
 });
