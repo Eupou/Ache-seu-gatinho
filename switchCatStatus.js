b9d9be5083e27168b929b12c10c5a011205377e2
Eupou <wesleywesleygomesgomes@gmail.com>
2023-02-14 10:58:44 -0300

    fix: change the function that removes the pagination in the form

--- switchCatStatus.js
+++ switchCatStatus.js
@@ -1,4 +1,4 @@
-function switchCatStatus() {
+function switchCatStatus(btn) {
   const btnCatStatus = document.getElementsByClassName("btn-cat-status");
   const btnFoundACat = document.getElementsByClassName("btn-found-a-cat");
   const btnFindYourCat = document.getElementsByClassName("btn-find-your-cat");
@@ -9,30 +9,34 @@ function switchCatStatus() {
   function switchStatus(e) {
     let btnClicked = e.target;
 
-    if (btnClicked == btnFindYourCat[0]) {
-      btnFindYourCat[0].classList.add("active-status-js");
-      pageFindYourCat[0].classList.remove("d-none");
+    if (btnClicked == btnCatStatus[0]) {
+      pageFindYourCat[0].classList.add("d-none");
+      pageFoundACat[0].classList.remove("d-none");
 
+      btnFindYourCat[0].classList.remove("active-status-js");
+      btnFoundACat[0].classList.add("active-status-js");
+
+      pagination[0].classList.remove("d-none");
+    } else {
+      btnFindYourCat[0].classList.add("active-status-js");
       btnFoundACat[0].classList.remove("active-status-js");
+
+      pageFindYourCat[0].classList.remove("d-none");
       pageFoundACat[0].classList.add("d-none");
 
       pagination[0].classList.add("d-none");
-    } else {
-      btnFindYourCat[0].classList.remove("active-status-js");
-      pageFindYourCat[0].classList.add("d-none");
-
-      btnFoundACat[0].classList.add("active-status-js");
-      pageFoundACat[0].classList.remove("d-none");
-
-      pagination[0].classList.remove("d-none");
     }
+  }
+
+  if (btn != undefined) {
+    switchStatus(btn);
   }
 
   Array.from(btnCatStatus).forEach((ele, i) => {
     if (i != 2) {
-      ele.addEventListener("click", switchStatus);
+      ele.addEventListener("click", switchCatStatus);
     }
   });
 }
 
-switchCatStatus();
+switchCatStatus(undefined);
